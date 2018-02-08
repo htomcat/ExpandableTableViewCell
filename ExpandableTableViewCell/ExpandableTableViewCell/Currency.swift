c253b2e520a8e257d58f370079d807c72b08b66f
Tomofumi Hamada <thamada@yahoo-corp.jp>
2018-02-08 09:59:12 +0900

    [add]Create a currency model

---
+++ ExpandableTableViewCell/ExpandableTableViewCell/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  ExpandableTableViewCell
+//
+//  Created by htomcat on 2018/02/08.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import Foundation
+
+struct Currency {
+    let country: String
+    let name: String
+    let code: String
+    let desc: String
+}
