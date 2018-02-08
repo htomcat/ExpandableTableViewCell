e0aacf0e6d09002a0c57d3df49d53ae5726ff0ed
Tomofumi Hamada <thamada@yahoo-corp.jp>
2018-02-08 09:53:42 +0900

    [add]Configure a table view

---
+++ ExpandableTableViewCell/ExpandableTableViewCell/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  ExpandableTableViewCell
+//
+//  Created by htomcat on 2018/02/07.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ExpandableTableViewCell/ExpandableTableViewCell/ViewController.swift
+++ ExpandableTableViewCell/ExpandableTableViewCell/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let identifier = "CustomTableViewCell"
+    var isExpandable = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: identifier)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? CustomTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = .red
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if isExpandable {
+            return 200
+        } else {
+            return 50
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        isExpandable = true
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
 }
 
